859bcc18fbc2d975593a5da60fb35d3f6aabe883
daseulsong <vates0328@gmail.com>
2020-11-26 11:05:59 +0900

    add: React testing of click counter by TDD

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Learn React Testing</h1>
+    <div data-test="component-app">
+      <button data-test="increment-button">Increment counter</button>
+      <h1 data-test="counterDisplay">The counter is currently</h1>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+// set up enzyme's react adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+test("renders button", () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
+});
+
+test("renders counter display", () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counterDisplay']");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test("counter starts at 0", () => {});
+
+test("clicking on button increments counter display", () => {});
